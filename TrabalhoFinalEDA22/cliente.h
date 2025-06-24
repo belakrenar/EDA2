8acf2a8e91250c1b63e6586ea2eef2862cb63da7
belakrenar <isakrenar@gmail.com>
2025-06-24 18:45:42 -0300

    Add files via upload

---
+++ TrabalhoFinalEDA22/cliente.h
@@ -0,0 +1,56 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
+
+#include <string>
+#include <iostream>
+#include <iostream>
+using namespace std;
+
+class Cliente{
+    static int idAtual;
+    int id;
+    string nome;
+    string cpf;
+
+    public:
+
+    Cliente(string x, string y)
+      : id(idAtual++), nome(x), cpf(y) {}
+
+    Cliente& operator=(const Cliente& outro) {
+      if(this != &outro){
+        id = outro.id;
+        nome = outro.nome;
+        cpf = outro.cpf;
+      }
+    return *this;
+    }
+
+    int getId(){
+      return id;
+    }
+
+    string getNome(){
+      return nome;
+    }
+
+    string getCpf(){
+      return cpf;
+    }
+
+    void toString(ostream& os){
+    os << "\nID: " << id << endl
+       << "Nome: " << nome << endl
+       << "CPF: " << cpf << endl;
+  }
+
+};
+
+inline ostream& operator<<(ostream& os, Cliente c) {
+    c.toString(os);
+    return os;
+}
+
+int Cliente::idAtual = 0;
+
+#endif
